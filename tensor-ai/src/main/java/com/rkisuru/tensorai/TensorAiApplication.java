7f851e140e57309d71595d065c91e0421a889ce6
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-12-20 23:16:45 +0530

    init project

---
+++ tensor-ai/src/main/java/com/rkisuru/tensorai/TensorAiApplication.java
@@ -0,0 +1,13 @@
+package com.rkisuru.tensorai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TensorAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TensorAiApplication.class, args);
+	}
+
+}
---
+++ tensor-ai/src/test/java/com/rkisuru/tensorai/TensorAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkisuru.tensorai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TensorAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

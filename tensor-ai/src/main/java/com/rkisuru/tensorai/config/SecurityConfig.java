9cc56c3c36a25c4a6f1e2dc0f9b9877bd3542329
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-12-23 14:01:31 +0530

    implement SecurityConfig

---
+++ tensor-ai/src/main/java/com/rkisuru/tensorai/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.rkisuru.tensorai.config;
+
+import com.rkisuru.tensorai.auth.OAuth2LoginSuccessHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsConfig config;
+    private final OAuth2LoginSuccessHandler successHandler;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(config.corsConfigurationSource()))
+                .authorizeHttpRequests(authorize-> authorize.requestMatchers("/api/v1/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login(oauth2-> {
+                    oauth2.successHandler(successHandler);
+                })
+                .build();
+    }
+}

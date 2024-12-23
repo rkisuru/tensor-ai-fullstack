dabe14f47202cef0fa96770bce8c65d20784a1e5
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-12-23 14:05:04 +0530

    implement Login page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import github from "../assets/github.png";
+import google from "../assets/google.png";
+
+const Login = () => {
+  const googleLogin = () => {
+    window.location.href = "http://localhost:8080/oauth2/authorization/google";
+  };
+
+  const githubLogin = () => {
+    window.location.href = "http://localhost:8080/oauth2/authorization/github";
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-neutral-900 dark:border-neutral-700 w-[400px] h-[500px] mx-auto">
+        <div className="p-4 sm:p-7">
+          <div className="text-center">
+            <h1 className="block text-2xl font-bold text-gray-800 dark:text-white">
+              Sign in
+            </h1>
+          </div>
+
+          <div className="mt-5">
+            <button
+              onClick={googleLogin}
+              type="button"
+              className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
+            >
+              <img
+                className="w-4 h-auto"
+                width="46"
+                height="47"
+                viewBox="0 0 46 47"
+                fill="none"
+                src={google}
+              />
+              Sign in with Google
+            </button>
+          </div>
+          <div className="mt-5">
+            <button
+              onClick={githubLogin}
+              type="button"
+              className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
+            >
+              <img
+                className="w-4 h-auto"
+                width="46"
+                height="47"
+                viewBox="0 0 46 47"
+                fill="none"
+                src={github}
+              />
+              Sign in with GitHub
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;

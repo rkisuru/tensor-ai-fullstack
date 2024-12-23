f5ff4072841e6ebb04b7f1a4aca1b73f7c861c43
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-12-23 14:07:06 +0530

    implement routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import Home from "./pages/Home";
-import ChatInput from "./components/ChatInput";
-import ChatResponse from "./components/ChatResponse";
-import { fetchResponse } from "./services/Api";
-import Question from "./components/Question";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 };
 

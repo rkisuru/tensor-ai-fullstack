cf3832cf1710794bdf0bc3ab2c9b06e1def54c68
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-12-23 14:06:14 +0530

    modify api to send cookie

--- client/src/services/Api.jsx
+++ client/src/services/Api.jsx
@@ -4,7 +4,11 @@ const API_URL = "http://localhost:8080/api/v1/tensor/ask";
 
 export const fetchResponse = async (question) => {
   try {
-    const response = await axios.post(API_URL, { question });
+    const response = await axios.post(
+      API_URL,
+      { question },
+      { withCredentials: true }
+    );
     return response.data;
   } catch (error) {
     console.error(error);

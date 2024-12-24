461058f62e4e5f4dca0d1084fd03511b24f15483
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-12-24 22:45:49 +0530

    add route for conversation component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import Menu from "./components/Menu.jsx";
+import Conversation from "./components/Conversation.jsx";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/conversation/:id" element={<Conversation />} />
       </Routes>
     </Router>
   );

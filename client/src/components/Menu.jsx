9a2e3af344165609479e974ed9d5626de5cb2b0f
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-12-27 23:09:37 +0530

    change styling

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Control from "../assets/control.png";
-import Logo from "../assets/logo.png";
+import Logo from "../assets/tensor-logo.png";
 import axios from "axios";
-import { fetchUserData } from "../services/Api";
+import { deleteChat, fetchUserData } from "../services/Api";
 import chat from "../assets/chat.png";
 import { useNavigate } from "react-router";
 import logout from "../assets/logout.png";
 import add from "../assets/add.png";
+import del from "../assets/trash-bin.png";
+import toast from "react-hot-toast";
 
 const Menu = () => {
   const [open, setOpen] = useState(true);
@@ -20,6 +22,14 @@ const Menu = () => {
   const handleLogout = async () => {
     try {
       window.location.href = "http://localhost:8080/logout";
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await deleteChat(id);
     } catch (error) {
       console.error(error);
     }
@@ -81,22 +91,15 @@ const Menu = () => {
       >
         <img
           src={Control}
-          className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
+          className={`absolute cursor-pointer -right-3 top-9 w-8 border-dark-purple
            border-2 rounded-full  ${!open && "rotate-180"}`}
           onClick={() => setOpen(!open)}
         />
-        <div className="flex gap-x-4 items-center mb-14">
-          <img
-            src={Logo}
-            className={`cursor-pointer duration-500 ${
-              open && "rotate-[360deg] mx-auto"
-            }`}
-          />
-        </div>
 
-        <hr />
-        <div className="h-[70%]">
-          <ul className={`${!open && "hidden"} pt-6 overflow-y-auto`}>
+        <div
+          className={`${!open && "hidden"} overflow-y-scroll h-[85%] py-5 pr-2`}
+        >
+          <ul className={`${!open && "hidden"}`}>
             <li className="my-5 justify-center overflow-hidden flex rounded-[100px] p-2 cursor-pointer bg-gray-200 hover:bg-gray-300 text-gray-800 text-sm max-w-[220px] mx-auto items-center gap-x-4">
               <button
                 onClick={newChat}
@@ -109,19 +112,30 @@ const Menu = () => {
             {Menus.slice()
               .reverse()
               .map((Menu, index) => (
-                <li
-                  key={Menu.id}
-                  className={`overflow-hidden flex rounded-[100px] p-2 cursor-pointer bg-gray-200 hover:bg-gray-300 text-gray-800 text-sm max-w-[220px] mx-auto items-center gap-x-4 
+                <div className="flex items-center">
+                  <li
+                    key={Menu.id}
+                    className={`overflow-hidden flex rounded-[100px] p-2 cursor-pointer bg-gray-200 hover:bg-gray-300 text-gray-800 text-sm w-[200px] max-w-[220px] mx-auto items-center gap-x-4 
             ${Menu.gap ? "mt-9" : "mt-2"} ${index === 0 && "bg-light-white"}`}
-                  onClick={() => handleMenuClick(Menu.id)}
-                >
-                  <img src={chat} alt="icon" className="w-4" />
-                  <span
-                    className={`${!open && "hidden"} origin-left duration-200`}
+                    onClick={() => handleMenuClick(Menu.id)}
                   >
-                    {Menu.title}
-                  </span>
-                </li>
+                    <img src={chat} alt="icon" className="w-4" />
+                    <span
+                      className={`${
+                        !open && "hidden"
+                      } origin-left duration-200`}
+                    >
+                      {Menu.title}
+                    </span>
+                  </li>
+                  <button
+                    key={Menu.id}
+                    onClick={() => handleDelete(Menu.id)}
+                    className="mt-2"
+                  >
+                    <img src={del} className="w-5" />
+                  </button>
+                </div>
               ))}
           </ul>
         </div>
